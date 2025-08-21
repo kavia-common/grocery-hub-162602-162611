cf8016155099cb5dea82cc4eefbaa6ed7acee3a0
Kavia AI <production@kavia.ai>
2025-08-21 08:36:48 +0000

    CheckPoint - cgf13916b0

---
+++ grocery_backend/src/main/java/com/example/demo/grocerybackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.grocerybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class grocerybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(grocerybackendApplication.class, args);
+	}
+
+}
---
+++ grocery_backend/src/test/java/com/example/demo/grocerybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grocerybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class grocerybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
